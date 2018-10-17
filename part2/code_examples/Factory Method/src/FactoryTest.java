3fa7b8fad16023c82546f00ef717ac07154a55b9
InspectorDeno <dennisdufback@gmail.com>
2018-10-17 17:02:18 +0200

    Add screenshots of test results

--- part2/code_examples/Factory Method/src/FactoryTest.java
+++ part2/code_examples/Factory Method/src/FactoryTest.java
@@ -8,7 +8,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class StatementTest {
+public class FactoryTest {
 	private PizzaStore ps;
 	private Pizza pizza;
 	
